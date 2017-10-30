9c3e1ec985f1b253c2b491a3fbe6c1be0294772d
Felipe Torres <felipe.torressepulveda@gmail.com>
2017-10-30 20:04:45 -0300

    adding a couple of demo components to show difference between a-box and a-entity

--- src/App.js
+++ src/App.js
@@ -9,6 +9,22 @@ class App extends Component {
         fog="type: linear; color#aab; far: 30; near: 0;"
       >
         <Environment />
+        <a-box
+          id="1"
+          depth="1"
+          height="1"
+          width="1"
+          position="0.5 0.5 0.5"
+        ></a-box>
+        <a-entity
+          id="2"
+          geometry={`
+            depth=1;
+            height=1;
+            width=1;
+          `}
+          position="1.5 0.5 1.5"
+        ></a-entity>
       </a-scene>
     );
   }
